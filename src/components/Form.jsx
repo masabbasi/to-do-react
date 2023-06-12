56d52cfe4ddd4f3516e373f82da16a3a3bc9dd9b
Your Namemasabbasi <masoud.abbasi.71@gmail.com>
2023-06-12 15:44:48 +0330

    All

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,97 +10,103 @@ const Form = () => {
     todo: "",
   });
 
-	const [updateTodoValue,setUpdateTodoValue] = useState({
-		todo:"",
-	})
+  const [updateTodoValue, setUpdateTodoValue] = useState({
+    todo: "",
+  });
 
   const [todo, setToDo] = useState([]);
 
   const submitHandler = (e) => {
     e.preventDefault();
+		if (inputValue.todo!==""){
     setToDo([...todo, inputValue]);
-		setInputValue({
-			todo:""
-		})
+    setInputValue({
+      todo: "",
+    });
+	}
+		console.log(todo);
   };
 
   const onChangeHandler = (input) => {
-		if (input.target.value.trim() !== ""){
-			setInputValue({
-				[input.target.name]: input.target.value.trim(),
-				id: todo.length,
-				completed: false,
-				edit:false,
-			});
-		}
+    if (input.target.value.trim()) {
+      setInputValue({
+        [input.target.name]: input.target.value.trim(),
+        id: todo.length,
+        completed: false,
+        edit: false,
+      });
+    }
   };
 
-	const completedHandler = (id)=>{
-	const updateToDo = todo.map((todo)=>{
-		if (todo.id===id) {
-			return {
-				...todo,
-				completed:!todo.completed,
-			}
-		}
-		return todo;
-	})
-	setToDo(updateToDo);
-	}
+  const completedHandler = (id) => {
+    const updateToDo = todo.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    });
+    setToDo(updateToDo);
+  };
 
-	const deleteHandler = (id)=>{
-		const updateToDo = todo.filter((todo)=>todo.id !== id)
+  const deleteHandler = (id) => {
+    const updateToDo = todo.filter((todo) => todo.id !== id);
 
-		setToDo(updateToDo)
-	}
+    setToDo(updateToDo);
+  };
 
-	const editTodoHandler = (id) =>{
-		const updateToDo = todo.map((todo)=>{
-			if (todo.id===id) {
-				return {
-					...todo,
-					edit:true,
-				}
-			}
-			return todo;
-		})
-		setToDo(updateToDo);
-	}
+  const editTodoHandler = (id) => {
+    const updateToDo = todo.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          edit: true,
+        };
+      }
+      return todo;
+    });
+    setToDo(updateToDo);
+  };
 
-	const updateValueHandler = (input)=>{
-		if (input.target.value.trim() !== ""){
-			setUpdateTodoValue({
-				todo: input.target.value.trim(),
-			});
-		}
-	}
+  const updateValueHandler = (input) => {
+    if (input.target.value.trim()!== "") {
+      setUpdateTodoValue({
+        todo: input.target.value.trim(),
+      });
+    }
+  };
 
-	const updateValue = (id) =>{
-		console.log(updateTodoValue);
-		const updateToDo = todo.map((todo)=>{
-			if (todo.id===id) {
-				return {
-					...todo,
-					todo: updateTodoValue.todo,
-					edit:false,
-				}
-			}
-			return todo;
-		})
-		setToDo(updateToDo)
-	}
-
-
+  const updateValue = (id) => {
+    console.log(updateTodoValue);
+    const updateToDo = todo.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          todo: updateTodoValue.todo,
+          edit: false,
+        };
+      }
+      return todo;
+    });
+    setToDo(updateToDo);
+    setUpdateTodoValue({ todo: "" });
+  };
 
   return (
     <>
-      <form action="#" className="flex items-center m-2" onSubmit={submitHandler}>
+      <form
+        action="#"
+        className="flex items-center m-2"
+        onSubmit={submitHandler}
+      >
         <input
           onChange={onChangeHandler}
           name="todo"
           type="text"
           value={inputValue.todo}
-					placeholder="Enter ToDo..."
+          placeholder="Enter ToDo..."
           className="w-72 bg-orange-400 p-2 m-1 rounded text-slate-200 font-bold placeholder-slate-50"
         />
         <button
@@ -110,36 +116,75 @@ const Form = () => {
           <BiMessageAdd />
         </button>
       </form>
-			<div className="flex justify-center items-center flex-col">
-			{todo.map((todo) => {
-        return (
-					<div key={todo.id} className={`w-72 ${todo.completed ? "bg-green-400" : "bg-slate-500"} flex justify-between items-center m-1 p-2 rounded`}>
-            <div className="flex justify-center items-center text-white">{todo.todo}</div>
-            <div className="flex justify-center items-center">
-							{todo.completed ? <MdDoneAll onClick={()=>{completedHandler(todo.id)}} className="mx-1 text-white text-sm" /> : <MdDone onClick={()=>{completedHandler(todo.id)}} className="mx-1 text-white text-sm" />
-							}
-							<FaTrash onClick={()=>{deleteHandler(todo.id)}} className="mx-1 text-white text-sm" />
-              <AiFillEdit onClick={()=>{editTodoHandler(todo.id)}} className="mx-1 text-white text-sm"/> {todo.edit?<>
-								<input
-								onChange={updateValueHandler}
-								name="updateTodoValue"
-								type="text"
-								value={updateTodoValue.todo}
-								placeholder="Update ToDo..."
-								className="w-36 bg-orange-400 p-2 m-1 rounded text-slate-200 text-sm font-bold placeholder-slate-50"
-							/><button 
-							onClick={()=>{updateValue(todo.id)}}
-							className="w-9 h-9 rounded p-2 text-xl flex justify-center items-center text-slate-100 bg-orange-400 hover:bg-orange-500 hover:text-2xl transition-all duration-300"
-						>
-							<MdOutlineUpdate />
-						</button>
-						</>
-								:null}
+      <div className="flex justify-center items-center flex-col">
+        {todo.map((todo) => {
+          return (
+            <div
+              key={todo.id}
+              className={`w-80 ${
+                todo.completed ? "bg-green-400" : "bg-slate-500"
+              } flex flex-col m-1 p-2 rounded`}
+            >
+              <div className="flex justify-between">
+                <div className="flex justify-center items-center text-white break-all">
+                  {todo.todo}
+                </div>
+                <div className="flex justify-center items-center">
+                  {todo.completed ? (
+                    <MdDoneAll
+                      onClick={() => {
+                        completedHandler(todo.id);
+                      }}
+                      className="mx-1 text-white text-sm"
+                    />
+                  ) : (
+                    <MdDone
+                      onClick={() => {
+                        completedHandler(todo.id);
+                      }}
+                      className="mx-1 text-white text-sm"
+                    />
+                  )}
+                  <FaTrash
+                    onClick={() => {
+                      deleteHandler(todo.id);
+                    }}
+                    className="mx-1 text-white text-sm"
+                  />
+                  <AiFillEdit
+                    onClick={() => {
+                      editTodoHandler(todo.id);
+                    }}
+                    className="mx-1 text-white text-sm"
+                  />
+                </div>
+              </div>
+              <div className="flex justify-center items-center">
+                {todo.edit ? (
+                  <>
+                    <input
+                      onChange={updateValueHandler}
+                      name="updateTodoValue"
+                      type="text"
+                      value={updateTodoValue.todo}
+                      placeholder="Update ToDo..."
+                      className="w-36 bg-orange-400 p-1 m-1 rounded text-slate-200 text-sm font-bold placeholder-slate-50"
+                    />
+                    <button
+                      onClick={() => {
+                        updateValue(todo.id);
+                      }}
+                      className="w-7 h-7 rounded p-2 text-3xl flex justify-center items-center text-slate-100 bg-orange-400 hover:bg-orange-500 hover:text-2xl transition-all duration-300"
+                    >
+                      <MdOutlineUpdate />
+                    </button>
+                  </>
+                ) : null}
+              </div>
             </div>
-          </div>
-        );
-      })}
-			</div>
+          );
+        })}
+      </div>
     </>
   );
 };
